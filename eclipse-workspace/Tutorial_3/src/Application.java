26772d862f5833f2382b5fe6ff454305b485f27e
Dave Nestor <dave.nestor@brunswickbowling.com>
2022-03-05 21:07:16 -0500

    Add files via upload

---
+++ eclipse-workspace/Tutorial_3/src/Application.java
@@ -0,0 +1,12 @@
+
+public class Application {
+	
+	public static void main(String[] args) {
+		int myInt = 7;
+		
+		String text = "hello";
+		
+		System.out.println(text);
+		
+	}
+}
---
+++ eclipse-workspace/helloworld/src/helloworld/application.java
@@ -0,0 +1,27 @@
+package helloworld;
+
+public class application {
+
+	public static void main(String[] args) {
+		
+		int i = 36; // whole numbers only 32 bit 
+		short s = 839;  // 16 bit number smaller numbers
+		long l = 8390 ;  // very large numbers
+		double d = 7.2356 ; // Floating point number
+		char c = 'd';  // characters only
+		boolean boo = false; // True / False only
+		byte by = 127; // up to 127 characters takes up less space
+		
+			System.out.println("Hello World this is the types of variables you can have in Java");
+			System.out.print("int ");
+			System.out.println(i);
+			System.out.println(s);
+			System.out.println(l);
+			System.out.println(d);
+			System.out.println(c);
+			System.out.println(boo);
+			System.out.println(by);
+			
+	}
+
+}
